ed59b7e128a7bb49c91f3487aabc0b888e23c6f8
Luke Cullen <75819270+retromonos@users.noreply.github.com>
2025-07-09 16:13:02 -0400

    tab system

---
+++ portfolio-v2/src/components/pages/Information.tsx
@@ -0,0 +1,18 @@
+import { MouseParallaxChild } from "react-parallax-mouse";
+
+export function Information()
+{
+    return(
+        <MouseParallaxChild className="text-xl/9 font-(family-name:--font-haas-grot-disp-55-roman) overflow-y-scroll">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia venenatis nisi, sed feugiat elit accumsan ornare. Donec nec ex tincidunt, molestie nisi nec, semper tortor. Mauris efficitur quis neque non cursus. Phasellus ornare pharetra orci et sodales. Donec cursus, quam eget venenatis consectetur, justo mi mollis urna, non feugiat justo odio id augue. Quisque tristique tincidunt nunc, sit amet fringilla arcu placerat eget. Aliquam egestas, ante nec maximus lacinia, leo leo ultrices dui, at mollis diam est nec tellus. Quisque vel laoreet risus, ac maximus enim. Donec blandit congue metus at mollis. Aliquam eget est scelerisque, porttitor justo et, pulvinar neque.
+            <br/>
+            Vivamus ultrices, massa nec iaculis laoreet, ante magna maximus lectus, quis sagittis augue urna ut eros. Integer vulputate nulla tempor arcu vulputate aliquet. Donec turpis lectus, molestie vitae pulvinar ut, gravida pretium tellus. Sed quis libero cursus, facilisis risus quis, dapibus lectus. Proin eleifend, ipsum in dignissim commodo, augue felis semper urna, nec eleifend tortor tellus ac dolor. Sed et gravida massa. Suspendisse purus ligula, consequat a lobortis sit amet, hendrerit a dui. Nullam maximus odio vitae diam congue, bibendum pulvinar ante porttitor. Cras egestas tortor sit amet purus pretium dignissim. Morbi porttitor lacus diam, in blandit nunc sodales nec. Sed sed dui risus. Donec ut lectus eu risus malesuada tempor. Mauris mattis, ipsum ut varius pharetra, ex sapien molestie nisi, ac placerat dui odio id leo.
+            <br/>
+            Morbi neque enim, lobortis at elit at, dictum aliquam sapien. Donec neque ante, blandit et gravida at, faucibus vitae enim. Quisque vel arcu interdum, eleifend purus eu, varius justo. Duis leo lectus, sodales sed nibh id, tincidunt accumsan ex. Praesent a efficitur odio, at fringilla orci. Pellentesque ornare at urna ut fermentum. Nullam quis neque nec libero imperdiet interdum a ut ipsum. Nullam aliquam ut lectus non eleifend. Etiam sodales augue enim, vitae mattis ipsum ultricies vel. Donec auctor nibh sed neque rhoncus feugiat. Aliquam a tincidunt elit, quis scelerisque eros.
+            <br/>
+            Morbi ultricies pharetra lectus nec egestas. Ut et odio et tellus placerat imperdiet vitae in nibh. Cras sollicitudin interdum tempus. Phasellus orci eros, vestibulum quis faucibus non, condimentum at metus. Sed posuere porttitor neque non porttitor. Donec felis sem, sagittis sed arcu et, luctus varius orci. Morbi nec tincidunt nisl. Nulla elementum posuere metus sit amet placerat.
+            <br/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec sodales metus, non vulputate tellus. Praesent feugiat libero a magna egestas iaculis. Donec ut metus at metus maximus tincidunt in et justo. Fusce posuere consectetur vulputate. Duis imperdiet mi vel blandit viverra. Donec eget urna magna. Nulla fringilla sit amet diam ut elementum. Sed pharetra risus velit, quis vehicula ipsum ultrices et. Nullam quis sapien eget dolor maximus scelerisque. Suspendisse a felis massa. Sed quis velit tempus, posuere velit et, feugiat est. Nunc iaculis arcu urna. 
+        </MouseParallaxChild>
+    )
+}
